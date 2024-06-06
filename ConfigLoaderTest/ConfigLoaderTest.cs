32cb55fe5c8a17b4124fb5bfad0096212477e839
Christophe Savard <christophe_savard@hotmail.ca>
2024-06-05 21:47:53 -0400

    Add test project

---
+++ ConfigLoaderTest/ConfigLoaderTest.cs
@@ -0,0 +1,17 @@
+﻿using ConfigLoader;
+using GeneratedCode;
+using UnityEngine;
+
+namespace ConfigLoaderTest;
+
+[KSPAddon(KSPAddon.Startup.Instantly, true), ConfigObject]
+public class ConfigLoaderTest : MonoBehaviour
+{
+    [ConfigField]
+    public string test;
+
+    private void Start()
+    {
+        Test.Foo();
+    }
+}
