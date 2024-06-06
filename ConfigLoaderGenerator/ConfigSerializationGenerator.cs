70167295d5bc14821544dbfa040ff5df5eea803e
Christophe Savard <christophe_savard@hotmail.ca>
2024-06-06 03:03:39 -0400

    Switch to incremental generator

---
+++ ConfigLoaderGenerator/ConfigSerializationGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using ConfigLoaderGenerator.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+/* ConfigLoader is distributed under CC BY-NC-SA 4.0 INTL (https://creativecommons.org/licenses/by-nc-sa/4.0/).                           *\
+ * You are free to redistribute, share, adapt, etc. as long as the original author (stupid_chris/Christophe Savard) is properly, clearly, *
+\* and explicitly credited, that you do not use this material to a commercial use, and that you distribute it under the same license.     */
+
+namespace ConfigLoaderGenerator;
+
+[Generator]
+public class ConfigSerializationGenerator : IIncrementalGenerator
+{
+    /// <inheritdoc />
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        IncrementalValuesProvider<ConfigTemplate> configDataProvider = context.SyntaxProvider.CreateSyntaxProvider(FilterConfigClasses, CreateConfigTemplate);
+        context.RegisterSourceOutput(configDataProvider, GenerateConfigMethods);
+    }
+
+    /// <summary>
+    /// Filter out classes with the ConfigObject attribute
+    /// </summary>
+    /// <param name="syntax">Inspected Syntax node</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns><see langword="true"/> if the <paramref name="syntax"/> is a class with a ConfigObject attribute, otherwise <see langword="false"/></returns>
+    /// <exception cref="OperationCanceledException">If the operation is cancelled through <paramref name="token"/></exception>
+    private static bool FilterConfigClasses(SyntaxNode syntax, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+        return syntax is ClassDeclarationSyntax classSyntax
+            && classSyntax.HasAttributeOfName(ConfigTemplate.ConfigObjectAttribute);
+    }
+
+    /// <summary>
+    /// Creates the <see cref="ConfigTemplate"/> associated with the current syntax context
+    /// </summary>
+    /// <param name="context">Current generator context</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>The created <see cref="ConfigTemplate"/></returns>
+    /// <exception cref="OperationCanceledException">If the operation is cancelled through <paramref name="token"/></exception>
+    private static ConfigTemplate CreateConfigTemplate(GeneratorSyntaxContext context, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+        return new ConfigTemplate(context);
+    }
+
+    private static void GenerateConfigMethods(SourceProductionContext context, ConfigTemplate template)
+    {
+    }
+}
---
+++ ConfigLoaderGenerator/ConfigTemplate.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+
+/* ConfigLoader is distributed under CC BY-NC-SA 4.0 INTL (https://creativecommons.org/licenses/by-nc-sa/4.0/).                           *\
+ * You are free to redistribute, share, adapt, etc. as long as the original author (stupid_chris/Christophe Savard) is properly, clearly, *
+\* and explicitly credited, that you do not use this material to a commercial use, and that you distribute it under the same license.     */
+
+namespace ConfigLoaderGenerator;
+
+public class ConfigTemplate
+{
+    public const string ConfigObjectAttribute = "ConfigObject";
+
+    public ConfigTemplate(GeneratorSyntaxContext context)
+    {
+    }
+
+    public string GenerateSource()
+    {
+        return string.Empty;
+    }
+}
